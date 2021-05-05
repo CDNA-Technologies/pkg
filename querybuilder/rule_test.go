df8084336d09ba0fdf96cf67ff088c7b924bc3fb
Aravind M <aravind@Administrators-MacBook-Pro.local>
2021-05-05 11:53:35 +0530

    Modify rule_test.go

--- querybuilder/rule_test.go
+++ querybuilder/rule_test.go
@@ -62,7 +62,7 @@ func TestRuleEvaluate(t *testing.T) {
 }
 
 func BenchmarkRuleEvaluate(b *testing.B) {
-	var ok interface{}
+	var ok bool
 
 	for i := 0; i < b.N; i++ {
 		for _, j := range ruleInputs {
@@ -70,5 +70,5 @@ func BenchmarkRuleEvaluate(b *testing.B) {
 		}
 	}
 
-	benchmarkRuleEvaluateResult = ok.(bool)
+	benchmarkRuleEvaluateResult = ok
 }
